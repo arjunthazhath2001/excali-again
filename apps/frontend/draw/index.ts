18ffe5046d8cf30d90a2fc411f2afd0ec4b57e02
Arjun T A <arjunta32@gmail.com>
2025-04-13 00:33:05 +0530

    v1 completed

--- apps/frontend/draw/index.ts
+++ apps/frontend/draw/index.ts
@@ -29,7 +29,7 @@ export async function initDraw(canvas: HTMLCanvasElement, roomId: string, socket
 
         if(message.type=="chat"){
             const parsedShape= JSON.parse(message.message)
-            existingShapes.push(parsedShape)
+            existingShapes.push(parsedShape.shape)
             clearCanvas(existingShapes,canvas,ctx)
         }
     }
@@ -112,7 +112,7 @@ async function getExistingShapes(roomId:string){
 
     const shapes= messages.map((x:{message:string})=>{
       const messageData= JSON.parse(x.message)  
-      return messageData; 
+      return messageData.shape; 
     })
 
     return shapes
